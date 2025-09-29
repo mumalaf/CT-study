eed93bed5a4947260018adbc9a6fee6eca55d1e2
Dayeon <132258173+mumalaf@users.noreply.github.com>
2025-09-29 20:34:19 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,8 +1,5 @@
 function solution(my_string, letter) {
     var answer = '';
-    for (let i=0; i<my_string.length; i++){
-        my_string=my_string.replace(letter,'');
-    }
-    answer = my_string;
+    answer = my_string.replaceAll(letter,'');
     return answer;
 }
